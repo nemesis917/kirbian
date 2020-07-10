9d23558481fa1c74b6ad48e1530e0446f0eb7af3
nemesis917 <nemesis917@hotmail.com>
2020-07-10 16:07:54 -0400

    Se agrego el Js para el manejo del select ppal del catalogo

--- public/js/sistema/catalogo/index.js
+++ public/js/sistema/catalogo/index.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
-    $
+    $("#seleccion").change(function(){
+        if ($(this).val() == "Seleccione...") {
+            return false;
+        } else {
+            alert( $(this).val() );
+        }
+    });
 
 
 
