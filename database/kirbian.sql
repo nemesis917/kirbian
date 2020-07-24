92d77f381b27a616a8ac56aa3b05506f44fac05e
nemesis917 <nemesis917@hotmail.com>
2020-07-24 02:26:55 -0400

    Se agrego la DB actualizada para que puedas ver todos los ususarios y las vistas de ejemplo, exactamente como la pagina kirbian.com

--- database/kirbian.sql
+++ database/kirbian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-07-2020 a las 21:29:51
+-- Tiempo de generación: 24-07-2020 a las 08:24:27
 -- Versión del servidor: 10.1.40-MariaDB
 -- Versión de PHP: 7.3.5
 
@@ -91,8 +91,11 @@ CREATE TABLE `comentarios_jero` (
 --
 
 INSERT INTO `comentarios_jero` (`id`, `comentario`, `puntuacion`, `visibilidad`, `jeroglificos_id`, `users_id`, `created_at`, `updated_at`) VALUES
-(2, 'sectetur adipiscing elit, nt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi', '0', 0, 1, 1, NULL, '2020-07-22 20:55:01'),
-(3, 'sectetur adipiscing elit, sed do eiusdidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi', '0', 0, 1, 1, NULL, NULL);
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', '0', 0, 1, 2, NULL, NULL),
+(2, 'labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation', '0', 0, 1, 2, NULL, NULL),
+(3, 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"', '0', 0, 1, 3, NULL, NULL),
+(4, 'Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?\"', '0', 0, 1, 2, NULL, NULL),
+(5, 'Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam,', '0', 0, 1, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -113,8 +116,12 @@ CREATE TABLE `descripcion` (
 --
 
 INSERT INTO `descripcion` (`id`, `descripcion`, `jeroglifico_id`, `created_at`, `updated_at`) VALUES
-(1, 'zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx', 1, '2020-07-22 20:11:21', '2020-07-22 20:11:21'),
-(2, 'dssddf sdfd dssddf sdfd dssddf sdfd', 2, '2020-07-22 23:18:55', '2020-07-22 23:18:55');
+(1, 'Placenta (?)(variante de Y24(?)). Antiguamente interpretada como la placenta de un animal, más probablemente una especie de canasta (?). El cordón umbilical era cortado con un cuchillo especial después de la expulsión de la placenta, ambos tenían asociaciones mágicas y se creía que estaban investidos de un alter ego espiritual o doble de la criatura. La placenta a menudo era enterrada en la casa o arrojada al Nilo para asegurar la supervivencia del niño, mientras que el cordón se dejaba secar y se conservaba para acompañar al individuo incluso a la tumba. La madre mordía su propia placenta ya que le otorgaba un gran valor simbólico (1. Llagostera E. El secreto de la felicidad. Muy Especial. 1998;(33):77-79. 2. Strouhal E. La vida en el antiguo Egipto. 1ª edición. Barcelona: Editorial Folio; 1994.).', 1, '2020-07-24 09:49:05', '2020-07-24 09:49:05'),
+(2, 'Variante de Aa1. Similar con el signo Y24 (?), ortografía simplificada de muchos signos circulares (N5, N9, N10, O50, X06, Aa1)', 2, '2020-07-24 09:59:18', '2020-07-24 09:59:18'),
+(3, 'Sustituto de varios signos: arcilla(N32), paquete de producto(V32), vendaje, embalsamamiento, cubierta(V38), granito (rojo), Elefantina(W7), madera de coníferas de origen extranjero(W41)', 3, '2020-07-24 10:05:55', '2020-07-24 10:05:55'),
+(4, 'Pústula supurativa (variante Aa2).', 4, '2020-07-24 10:08:23', '2020-07-24 10:08:23'),
+(5, 'Pústula supurativa (variante de Aa2).', 5, '2020-07-24 10:11:00', '2020-07-24 10:11:00'),
+(6, 'Copa utilizada como lámpara (variante de W10a).', 6, '2020-07-24 10:13:07', '2020-07-24 10:13:07');
 
 -- --------------------------------------------------------
 
@@ -131,6 +138,19 @@ CREATE TABLE `imagenes_comentario_jero` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `imagenes_comentario_jero`
+--
+
+INSERT INTO `imagenes_comentario_jero` (`id`, `ruta_img_jero`, `visibilidad`, `jeroglificos_id`, `users_id`, `created_at`, `updated_at`) VALUES
+(1, 'imagenes/comentario/1.jpg', 0, 1, 2, NULL, NULL),
+(2, 'imagenes/comentario/2.jpg', 0, 1, 2, NULL, NULL),
+(3, 'imagenes/comentario/3.jpg', 0, 1, 1, NULL, NULL),
+(4, 'imagenes/comentario/4.jpg', 0, 1, 3, NULL, NULL),
+(5, 'imagenes/comentario/5.jpg', 0, 1, 1, NULL, NULL),
+(6, 'imagenes/comentario/6.jpg', 0, 1, 2, NULL, NULL),
+(7, 'imagenes/comentario/7.jpg', 0, 1, 1, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -152,12 +172,24 @@ CREATE TABLE `imagenes_jeroglificos` (
 --
 
 INSERT INTO `imagenes_jeroglificos` (`id`, `ruta_imagen`, `referencia`, `jeroglifico_id`, `created_at`, `updated_at`) VALUES
-(1, 'imagenes/catalogo/55/976-jero-55-923.png', '1', 1, '2020-07-22 20:11:21', '2020-07-22 20:11:21'),
-(2, 'system/no-foto.jpg', '2', 1, '2020-07-22 20:11:21', '2020-07-22 20:11:21'),
-(3, 'system/no-foto.jpg', '3', 1, '2020-07-22 20:11:21', '2020-07-22 20:11:21'),
-(4, 'imagenes/catalogo/55/734-jero-55-606.png', '1', 2, '2020-07-22 23:18:57', '2020-07-22 23:18:57'),
-(5, 'system/no-foto.jpg', '2', 2, '2020-07-22 23:18:57', '2020-07-22 23:18:57'),
-(6, 'system/no-foto.jpg', '3', 2, '2020-07-22 23:18:57', '2020-07-22 23:18:57');
+(1, 'imagenes/catalogo/80/471-jero-80-801.png', '1', 1, '2020-07-24 09:49:05', '2020-07-24 09:49:05'),
+(2, 'imagenes/catalogo/80/750-jero-80-547.png', '2', 1, '2020-07-24 09:49:05', '2020-07-24 09:49:05'),
+(3, 'imagenes/catalogo/80/649-jero-80-554.png', '3', 1, '2020-07-24 09:49:05', '2020-07-24 09:49:05'),
+(4, 'imagenes/catalogo/80/299-jero-80-935.png', '1', 2, '2020-07-24 09:59:18', '2020-07-24 09:59:18'),
+(5, 'imagenes/catalogo/80/422-jero-80-480.png', '2', 2, '2020-07-24 09:59:18', '2020-07-24 09:59:18'),
+(6, 'imagenes/catalogo/80/456-jero-80-576.png', '3', 2, '2020-07-24 09:59:18', '2020-07-24 09:59:18'),
+(7, 'imagenes/catalogo/80/81-jero-80-500.png', '1', 3, '2020-07-24 10:05:55', '2020-07-24 10:05:55'),
+(8, 'imagenes/catalogo/80/791-jero-80-586.png', '2', 3, '2020-07-24 10:05:55', '2020-07-24 10:05:55'),
+(9, 'imagenes/catalogo/80/186-jero-80-937.png', '3', 3, '2020-07-24 10:05:55', '2020-07-24 10:05:55'),
+(10, 'imagenes/catalogo/80/670-jero-80-934.png', '1', 4, '2020-07-24 10:08:24', '2020-07-24 10:08:24'),
+(11, 'imagenes/catalogo/80/912-jero-80-288.png', '2', 4, '2020-07-24 10:08:24', '2020-07-24 10:08:24'),
+(12, 'imagenes/catalogo/80/450-jero-80-515.png', '3', 4, '2020-07-24 10:08:24', '2020-07-24 10:08:24'),
+(13, 'imagenes/catalogo/80/101-jero-80-82.png', '1', 5, '2020-07-24 10:11:00', '2020-07-24 10:11:00'),
+(14, 'imagenes/catalogo/80/292-jero-80-921.png', '2', 5, '2020-07-24 10:11:00', '2020-07-24 10:11:00'),
+(15, 'imagenes/catalogo/80/660-jero-80-172.png', '3', 5, '2020-07-24 10:11:00', '2020-07-24 10:11:00'),
+(16, 'imagenes/catalogo/80/464-jero-80-46.png', '1', 6, '2020-07-24 10:13:07', '2020-07-24 10:13:07'),
+(17, 'imagenes/catalogo/80/991-jero-80-425.png', '2', 6, '2020-07-24 10:13:07', '2020-07-24 10:13:07'),
+(18, 'system/no-foto.jpg', '3', 6, '2020-07-24 10:13:07', '2020-07-24 10:13:07');
 
 -- --------------------------------------------------------
 
@@ -168,7 +200,7 @@ INSERT INTO `imagenes_jeroglificos` (`id`, `ruta_imagen`, `referencia`, `jerogli
 CREATE TABLE `jeroglificos` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `gandiner` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `transliteracion` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `transliteracion` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL,
   `sentido` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `nombre_usuario` varchar(70) COLLATE utf8mb4_unicode_ci NOT NULL,
   `comentario` text COLLATE utf8mb4_unicode_ci,
@@ -183,8 +215,12 @@ CREATE TABLE `jeroglificos` (
 --
 
 INSERT INTO `jeroglificos` (`id`, `gandiner`, `transliteracion`, `sentido`, `nombre_usuario`, `comentario`, `catalogo_id`, `visibilidad`, `created_at`, `updated_at`) VALUES
-(1, 'zxzxvxzvzx', 'zxzxv xzvzx', 'zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx', 'admin admin', 'zxzxv xzvzx  zxzxv xzvzx  zxzxv xzvzx', 55, 1, '2020-07-22 20:11:21', '2020-07-22 20:11:21'),
-(2, 'dssddf sdfd', 'dssddf sdfd dssddf sdfd', 'dssddf sdfd dssddf sdfd dssddf sdfd dssddf sdfd', 'admin admin', 'dssddf sdfd dssddf sdfd dssddf sdfd dssddf sdfd', 55, 1, '2020-07-22 23:18:55', '2020-07-22 23:18:55');
+(1, 'Aa1', 'x', '-', 'admin admin', NULL, 80, 1, '2020-07-24 09:49:05', '2020-07-24 09:49:05'),
+(2, 'Aa1a', 'x', '-', 'admin admin', NULL, 80, 1, '2020-07-24 09:59:18', '2020-07-24 09:59:18'),
+(3, 'Aa2', 'Xpw / gA / Hsb', 'Enfermedad, estados del cuerpo, hinchado, olor, excremento, sucio, 1/4 ...', 'admin admin', NULL, 80, 1, '2020-07-24 10:05:55', '2020-07-24 10:05:55'),
+(4, 'Aa3', 'Hsb', 'Secreciones corporales, olor.', 'admin admin', NULL, 80, 1, '2020-07-24 10:08:23', '2020-07-24 10:08:23'),
+(5, 'Aa3', 'Hbs', 'Secreciones corporales, olor', 'admin admin', NULL, 80, 1, '2020-07-24 10:11:00', '2020-07-24 10:11:00'),
+(6, 'Aa4', 'iab / sxw / wsx / ab', '-', 'admin admin', NULL, 80, 1, '2020-07-24 10:13:07', '2020-07-24 10:13:07');
 
 -- --------------------------------------------------------
 
@@ -203,11 +239,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(106, '2014_10_12_000000_create_users_table', 1),
-(107, '2014_10_12_100000_create_password_resets_table', 1),
-(108, '2020_07_10_043508_catalogo', 1),
-(109, '2020_07_11_044749_jeroglificos', 1),
-(110, '2020_07_11_054259_comentarios_jero', 1);
+(136, '2014_10_12_000000_create_users_table', 1),
+(137, '2014_10_12_100000_create_password_resets_table', 1),
+(138, '2020_07_10_043508_catalogo', 1),
+(139, '2020_07_11_044749_jeroglificos', 1),
+(140, '2020_07_11_054259_comentarios_jero', 1);
 
 -- --------------------------------------------------------
 
@@ -234,7 +270,7 @@ CREATE TABLE `users` (
   `email` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `level` enum('administrador','moderador') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'moderador',
+  `level` enum('administrador','moderador','usuario') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'usuario',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -245,7 +281,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `lastname`, `email`, `email_verified_at`, `password`, `level`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin', 'admin@admin', NULL, '$2y$10$g6QnZiMvovrwIq.g7w6LQ.nzEQohdita3Qhk5u5KmVUGd8dMWAgsy', 'administrador', 'KMUMe4nWFezFv4xbCdEttRP3P2346ROqHFTe3GuC8zHtgyQI5Ls8DLbNk5hU', NULL, NULL);
+(1, 'admin', 'admin', 'admin@admin', NULL, '$2y$10$g6QnZiMvovrwIq.g7w6LQ.nzEQohdita3Qhk5u5KmVUGd8dMWAgsy', 'administrador', 'KMUMe4nWFezFv4xbCdEttRP3P2346ROqHFTe3GuC8zHtgyQI5Ls8DLbNk5hU', NULL, NULL),
+(2, 'usuario', 'usuario', 'usuario@usuario', NULL, '$2y$10$g6QnZiMvovrwIq.g7w6LQ.nzEQohdita3Qhk5u5KmVUGd8dMWAgsy', 'usuario', NULL, NULL, NULL),
+(3, 'moderador', 'moderador', 'moderador@moderador', NULL, '$2y$10$g6QnZiMvovrwIq.g7w6LQ.nzEQohdita3Qhk5u5KmVUGd8dMWAgsy', 'moderador', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -255,7 +293,7 @@ INSERT INTO `users` (`id`, `name`, `lastname`, `email`, `email_verified_at`, `pa
 --
 CREATE TABLE `vw_catalogo_jeroglifico` (
 `id` bigint(20) unsigned
-,`transliteracion` varchar(30)
+,`transliteracion` varchar(200)
 ,`gandiner` varchar(12)
 ,`ruta_imagen` varchar(100)
 ,`visibilidad` tinyint(1)
@@ -301,7 +339,7 @@ CREATE TABLE `vw_ver_img_paleografica` (
 --
 CREATE TABLE `vw_ver_jeroglifico` (
 `id` bigint(20) unsigned
-,`transliteracion` varchar(30)
+,`transliteracion` varchar(200)
 ,`gandiner` varchar(12)
 ,`descripcion` text
 ,`significado` text
@@ -425,43 +463,43 @@ ALTER TABLE `catalogo`
 -- AUTO_INCREMENT de la tabla `comentarios_jero`
 --
 ALTER TABLE `comentarios_jero`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `descripcion`
 --
 ALTER TABLE `descripcion`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes_comentario_jero`
 --
 ALTER TABLE `imagenes_comentario_jero`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `imagenes_jeroglificos`
 --
 ALTER TABLE `imagenes_jeroglificos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `jeroglificos`
 --
 ALTER TABLE `jeroglificos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
